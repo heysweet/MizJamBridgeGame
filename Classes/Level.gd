67232d8837bbb21def263e7349ab4a88e0291bce
Dan Wilkerson <notdanwilkerson@gmail.com>
2020-08-15 17:41:25 -0400

    Lvel with nav

--- Classes/Level.gd
+++ Classes/Level.gd
@@ -7,10 +7,15 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+  # Creates carts
+  
   
   pass # Replace with function body.
 
+func change():
+  
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#  pass
+func _process(delta):
+  
+  pass
