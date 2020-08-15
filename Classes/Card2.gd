00307b11bdffa429a80521d0eb3bcb524efb54b1
Dan Wilkerson <notdanwilkerson@gmail.com>
2020-08-15 17:00:54 -0400

    renderable refactor

--- Classes/Card2.gd
+++ Classes/Card2.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends "res://Classes/Renderable2.gd"
 
 enum Suit {HEART = 0, DIAMOND, CLUB, SPADE}
 enum Rank {ACE = 1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING}
@@ -8,8 +8,6 @@ tool
 # Ace of Hearts
 var min_row = 16
 var min_col = 20
-var width = 16
-var height = 16
 
 export(Suit) var suit = Suit.HEART setget set_suit
 export(Rank) var rank = Rank.ACE setget set_rank
@@ -20,22 +18,12 @@ export(Rank) var rank = Rank.ACE setget set_rank
 
 func set_suit(new_suit):
   suit = new_suit
-  draw_me()
-  #.x = (min_row + new_suit) * 16
- # if $Renderable:
-   # $Renderable.set_row(min_row + new_suit)
+  set_row(min_row + suit)
   
 func set_rank(new_rank):
   rank = new_rank
-  draw_me()
-
-func draw_me():
-  var x_pos = (min_col + rank - 1) * 16
-  var y_pos = (min_row + suit) * 16
-  region_rect = Rect2(x_pos, y_pos, width, height)
-  #if $Renderable:
-    #$Renderable.set_col(min_col + new_rank - 1)
-
+  set_col(min_col + rank - 1)
+  
 # Called when the node enters the scene tree for the first time.
 func _ready():
   pass # Replace with function body.
