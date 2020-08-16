840d7c3e88c896644f8b737cd00c1087f77e62b9
Andrew Sweet <andrewmsweet@gmail.com>
2020-08-16 14:29:37 -0400

    Adding Movable class

---
+++ Classes/Movable.gd
@@ -0,0 +1,38 @@
+extends "res://Classes/Renderable2.gd"
+
+export var tile_row : int = 0 setget set_tile_row
+export var tile_col : int = 0 setget set_tile_col
+var target_position = Vector2()
+var movement_tween : Tween
+
+tool
+    
+func set_tile_col(col):
+  tile_col = col
+  target_position.x = 16 * tile_col
+  if Engine.editor_hint:
+    position = target_position
+  
+func set_tile_row(row):
+  tile_row = row
+  target_position.y = 16 * tile_row
+  if Engine.editor_hint:
+    position = target_position
+
+func do_move(move_vector):
+  if move_vector.x == 0 and move_vector.y == 0:
+    return
+  if move_vector.x != 0:
+    set_tile_col(tile_col + move_vector.x)
+  else:
+    set_tile_row(tile_row + move_vector.y)
+  update_tween(move_vector)
+
+func update_tween(move_vector : Vector2):
+  var tween = $Tween
+  tween.interpolate_property(self, "position",
+          position, target_position, 0.130,
+          Tween.EASE_IN_OUT, Tween.EASE_IN_OUT)
+  tween.interpolate_property(self, "rotation", 0.15 * -move_vector.x, 0, 0.400, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
+  tween.interpolate_property(self, "scale", Vector2(1.1, 1.1), Vector2(1, 1), 0.300, Tween.TRANS_BACK, Tween.EASE_OUT)
+  tween.start()
--- Classes/Player.gd
+++ Classes/Player.gd
@@ -1,16 +1,9 @@
-extends "res://Classes/Renderable2.gd"
-
-export var tile_row : int = 0 setget set_tile_row
-export var tile_col : int = 0 setget set_tile_col
+extends "res://Classes/Movable.gd"
 
 var last_key_press = 0
 var debounce_millis = 80
-var target_position = Vector2()
-var movement_tween : Tween
 
 signal time_step
-
-tool
 
 func _input(ev):
   if ev is InputEventKey and ev.is_pressed() and not ev.echo:
@@ -38,37 +31,10 @@ func try_move(move_vector):
   last_key_press = now
   if can_move(move_vector):
     do_move(move_vector)
-    
-func set_tile_col(col):
-  tile_col = col
-  target_position.x = 16 * tile_col
-  if Engine.editor_hint:
-    position = target_position
-  
-func set_tile_row(row):
-  tile_row = row
-  target_position.y = 16 * tile_row
-  if Engine.editor_hint:
-    position = target_position
-
-func update_tween(move_vector : Vector2):
-  var tween = $Tween
-  tween.interpolate_property(self, "position",
-          position, target_position, 0.130,
-          Tween.EASE_IN_OUT, Tween.EASE_IN_OUT)
-  tween.interpolate_property(self, "rotation", 0.15 * -move_vector.x, 0, 0.400, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
-  tween.interpolate_property(self, "scale", Vector2(1.1, 1.1), Vector2(1, 1), 0.300, Tween.TRANS_BACK, Tween.EASE_OUT)
-  tween.start()
 
 func do_move(move_vector):
-  if move_vector.x == 0 and move_vector.y == 0:
-    return
-  if move_vector.x != 0:
-    set_tile_col(tile_col + move_vector.x)
-  else:
-    set_tile_row(tile_row + move_vector.y)
+  .do_move(move_vector)
   emit_signal("time_step")
-  update_tween(move_vector)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
