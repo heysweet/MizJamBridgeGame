7b2d332530be44624785a373fbee68dd70958a4c
Dan Wilkerson <notdanwilkerson@gmail.com>
2020-08-16 18:44:52 -0400

    Added navigation call

--- Classes/Layout/Level.gd
+++ Classes/Layout/Level.gd
@@ -6,29 +6,30 @@ extends Node2D
 enum Suit {HEART = 0, DIAMOND, CLUB, SPADE}
 const RedCities = []
 const BlueCities = []
-var navigation2d : Navigation2D;
 
 func _init():
-  navigation2d = Navigation2D.new()
   pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  print(navigation2d)
+
   for city in $Cities.get_children():
     if city.suit == Suit.HEART || city.suit == Suit.DIAMOND:
       RedCities.append(city)
     else:
       BlueCities.append(city)
   for cart in $Carts.get_children():
-    print(cart)
-    #var cart_suit = cart.get_suit()
+    print(cart.position)
+    print(RedCities[0].position)
+    var path = $Navigation2D.get_simple_path(cart.position, RedCities[0].position,true)
+    print(path)
+    cart.set_path_to(path)
+
    # if cart_suit == Suit.HEART || cart_suit == Suit.DIAMOND:
       #print("should work")
+
   pass # Replace with function body.
 
-func on_step():     
-  pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- Classes/MovingObjects/Cart.gd
+++ Classes/MovingObjects/Cart.gd
@@ -6,16 +6,19 @@ var time_passed = 0
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var path_to_city = [Vector2(1,1), Vector2(1,1)]
+var path_to_city setget set_path_to
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+  path_to_city = []
   pass # Replace with function body.
   
 func _process(time):
   pass
 
+func set_path_to(arr):
+  path_to_city = arr
+
 func time_step():
   if len(path_to_city) > 0:
     do_move(path_to_city.pop_back())
-
