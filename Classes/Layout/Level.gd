7116853982713349ee2af7c5018a6fc025bf7516
Dan Wilkerson <notdanwilkerson@gmail.com>
2020-08-16 17:59:58 -0400

    Fixed valley.png and added some level stubs

--- Classes/Layout/Level.gd
+++ Classes/Layout/Level.gd
@@ -2,13 +2,30 @@ extends Node2D
 
 # Declare member variables here. Examples:
 # var a = 2
-# var b = "text"
+# var b = "text
+enum Suit {HEART = 0, DIAMOND, CLUB, SPADE}
+const RedCities = []
+const BlueCities = []
+var navigation2d : Navigation2D;
 
+func _init():
+  navigation2d = Navigation2D.new()
+  pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+  print(navigation2d)
+  for city in $Cities.get_children():
+    if city.suit == Suit.HEART || city.suit == Suit.DIAMOND:
+      RedCities.append(city)
+    else:
+      BlueCities.append(city)
+  for cart in $Carts.get_children():
+    print(cart)
+    #var cart_suit = cart.get_suit()
+   # if cart_suit == Suit.HEART || cart_suit == Suit.DIAMOND:
+      #print("should work")
   pass # Replace with function body.
-
 
 func on_step():     
   pass
--- Classes/Layout/Navigation2D.gd
+++ Classes/Layout/Navigation2D.gd
@@ -8,7 +8,7 @@ extends Navigation2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-
+  print("ready")
   pass # Replace with function body.
 
 
