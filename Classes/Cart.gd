039f7c31482278bae2a02a762743e2b221e36901
Andrew Sweet <andrewmsweet@gmail.com>
2020-08-16 12:08:39 -0400

    Replacing Card with Card2

--- Classes/Cart.gd
+++ Classes/Cart.gd
@@ -3,7 +3,6 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var Level level
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- Classes/Level.gd
+++ Classes/Level.gd
@@ -13,9 +13,8 @@ func _ready():
   pass # Replace with function body.
 
 func change():
-  
+  pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-  
   pass
