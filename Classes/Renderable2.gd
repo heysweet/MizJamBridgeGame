035987753f579bc1b289a4e0e06d8d2c105ab9e1
Dan Wilkerson <notdanwilkerson@gmail.com>
2020-08-16 16:40:27 -0400

    Renderable tweaks

--- Classes/Renderable2.gd
+++ Classes/Renderable2.gd
@@ -2,13 +2,19 @@ extends Node2D
 
 tool
 
-var width = 16
-var height = 16
-var resource_pack = preload("res://1bitpack_kenney_1.1/Tilesheet/monochrome_transparent_packed.png")
-var sprite = Sprite.new()
+const width = 16
+const height = 16
+const resource_pack = preload("res://1bitpack_kenney_1.1/Tilesheet/monochrome_transparent_packed.png")
+var sprite : Sprite;
 
 export var row : int = 0 setget set_row
 export var col : int = 0 setget set_col
+
+func _init():
+  sprite = Sprite.new()
+  sprite.texture = resource_pack
+  sprite.region_enabled = true
+  draw_me()
 
 func set_row(new_val):
   row = new_val
@@ -21,10 +27,8 @@ func set_col(new_val):
 func draw_me():
   var x_pos = col * 16
   var y_pos = row  * 16
-  sprite.region_enabled = true
-  sprite.texture = resource_pack
   sprite.region_rect = Rect2(x_pos, y_pos, width, height)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  draw_me()
+  pass
