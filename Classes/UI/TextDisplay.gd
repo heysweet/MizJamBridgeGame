4cb567733ec4a9bc8a8e093eaee53cf7aa55198a
Andrew Sweet <andrewmsweet@gmail.com>
2020-08-22 18:53:53 -0400

    Font progress

--- Classes/UI/TextDisplay.gd
+++ Classes/UI/TextDisplay.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Node2D
 
 export var display_string : String = "Test text." setget set_message
 export var start_pos : Vector2 = Vector2.ZERO setget set_position
@@ -30,7 +30,7 @@ var char_map = {
 
 var min_row = 18
 var min_col = 35
-var num_cols = 12
+var num_cols = 13
 var a_ord = 'a'.ord_at(0)
 
 var renderable = null
@@ -38,28 +38,30 @@ var renderable = null
 tool
 
 func set_max_width(width : int):
-  max_width = width
-  render_text()
+  if max_width != width:
+    max_width = width
+    render_text()
 
 func set_position(pos : Vector2, bool_value=true):
-  start_pos = pos
-  if renderable == null:
-    renderable = load("res://Classes/Renderable.tscn")
-  render_text()
+  if pos != start_pos:
+    start_pos = pos
+    render_text()
 
 func set_message(new_message : String):
-  display_string = new_message
-  if renderable == null:
-    renderable = load("res://Classes/Renderable.tscn")
-  render_text()
+  if display_string != new_message:
+    display_string = new_message
+    render_text()
 
 func create_renderable(row_col, pos):
+  if renderable == null:
+    renderable = load("res://Classes/Renderable.tscn")
   var new_renderable = renderable.instance()
   .add_child(new_renderable)
   new_renderable.set_row(row_col.y)
   new_renderable.set_col(row_col.x)
   new_renderable.position = pos
-  print(new_renderable.position)
+  print(row_col)
+  print(pos)
 
 func render_character(character : String, pos : Vector2):
   var c = character.to_lower()
@@ -74,8 +76,11 @@ func render_character(character : String, pos : Vector2):
     var index = c.ord_at(0) - a_ord
     var row_num = floor(index / num_cols) + min_row
     var col_num = (index % num_cols) + min_col
-    width = 8
-    row_col = Vector2(row_num, col_num)
+    width = 10
+    row_col = Vector2(col_num, row_num)
+  if max_width != -1 and pos.x + width > max_width:
+    pos.x = start_pos.x
+    pos.y += 12
   create_renderable(row_col, pos)
   return Vector2(pos.x + width, pos.y)
 
@@ -93,6 +98,7 @@ func render_text():
 # Called when the node enters the scene tree for the first time.
 func _ready():
   render_text()
+  create_renderable(Vector2(1, 1), Vector2(10, 10))
   pass # Replace with function body.
 
 
