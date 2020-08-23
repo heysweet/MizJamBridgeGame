a8677a308c3dae3cd1211127c5a87f46017c08b1
Andrew Sweet <andrewmsweet@gmail.com>
2020-08-23 14:54:55 -0400

    Audio tweaking + initialization. Island2 has two exits

--- Classes/Levels/Control.gd
+++ Classes/Levels/Control.gd
@@ -5,7 +5,9 @@ var min_time_sample_play = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  pass # Replace with function body.
+  _on_MasterSlider_value_changed(600)
+  _on_MusicSlider_value_changed(700)
+  _on_SoundFXSlider_value_changed(320)
 
 func get_db(value : int):
   return 20.0 * (log(value * 0.001) / log(10))
